f2ad135b15019d698fab2e1c1f315331d9768f75
BekzodQodirovdev <oldakkaun4@gmail.com>
2025-01-10 17:05:14 +0500

    update

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
+import express from "express";
 const PORT = process.env.PORT || 4070;
 
 const app = express();
 
-app.use('/greet', (req, res) => {
-  res.send('SALOMLAR!');
+app.use("/greet", (req, res) => {
+    res.send("SALOMLAR!");
 });
 
-app.use('/users', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: 'vali',
-      age: 22,
-      isMerred: false,
-    },
-  ]);
+app.use("/users", (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "vali",
+            age: 22,
+            isMerred: false,
+        },
+    ]);
 });
 
-app.use('/app', (req, res) => {
-  res.send('APPLOCATION!');
+app.use("/app", (req, res) => {
+    res.send("APPLOCATION!");
 });
 
 app.use((req, res) => {
-  res.send('ASSALOMU ALAYKUM!.');
+    res.send("ASSALOMU ALAYKUM!.");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
